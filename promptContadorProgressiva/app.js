1d8724b7eaca87151c3acbc52ac9101acd31d1c0
M4lf0r <64777390+m4lf0r@users.noreply.github.com>
2024-03-29 09:40:07 -0300

    mergulhe em programação com JavaScript
    Exiba mensagens na tela de forma interativa
    Utilize variáveis no desenvolvimento de software
    Ingresse no mundo de desenvolvimento seguindo boas práticas de programação
    Desenvolva uma aplicação do início ao fim, inspirada no mundo real
    Aprenda a adaptar soluções desenvolvidas pela linguagem em seus programas de software

---
+++ promptContadorProgressiva/app.js
@@ -0,0 +1,7 @@
+let qtdNumeros = prompt("Digite um número para fazer a contagem progressiva");
+let contador = 0;
+
+while(contador <= qtdNumeros) {
+    console.log(contador);
+    contador++;
+}
---
+++ promptContadorRegressivo/app.js
@@ -0,0 +1,6 @@
+let qtdNumeros = prompt("Digite um número para fazer a contagem regressiva")
+
+while(qtdNumeros >= 0) {
+    console.log(qtdNumeros);
+    qtdNumeros--;
+}
